a8c3a41ad448a9a41ba1deeedcfbb060dc2ea9d0
Jack Palfrey <jack.palf@gmail.com>
2018-05-22 15:27:05 +0100

    exiting with code 1 on failure

--- src/assertions.js
+++ src/assertions.js
@@ -2,7 +2,7 @@ function assertEquals (expected) {
   return (actual) => new Promise((resolve, reject) =>
     (expected === actual)
       ? resolve(actual)
-      : reject({ message: `expected (${expected}) did not match actual (${actual})` })
+      : reject({ message: `expected (${expected}); actual (${actual})` })
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ const baseUrl = 'http://localhost:3000/'
 
 function runTest (page, someTest) {
   return someTest(page)
-    .then((v) => v)
-    .catch((e) => e)
+    .then((v) => ({ output: v, result: true}))
+    .catch((e) => ({ output: e, result: false}))
 }
 
 function createDirectory (dir) {
@@ -23,6 +23,7 @@ function createDirectory (dir) {
   const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']})
   const page = await browser.newPage()
   const outcome = await runTest({ page, baseUrl }, orderDeclinedTest)
-  console.log(outcome)
+  console.log(outcome.output);
   await browser.close()
+  process.exit(outcome.result ? 0 : 1);
 })()
